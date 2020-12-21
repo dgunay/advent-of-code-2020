ab65c70370e82dc1bbde73c69847589ca1bbd2d5
Devin gunay <devingunay@gmail.com>
2020-12-20 19:16:16 -0800

    setup for part 1 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+mod day1;
+
+fn load_day_input(path: &Path) -> Result<String, Error> {
+    std::fs::read_to_string(path).map_err(
+        |_| Error::InputError(format!("Couldn't load input at {}", path.to_str().unwrap()))
+    )
+}
+
+#[derive(Debug)]
+enum Error {
+    SolutionError(i32),
+    InputError(String),
+}
+
+fn main() -> Result<(), Error> {
+    // TODO: parse a number from cli args
+    let path_str = std::env::args()
+        .nth(1)
+        .ok_or(Error::InputError("No path given".to_string()))?;
+
+    // open that day's input
+    let path = Path::new(path_str.as_str());
+    let input = load_day_input(path)?;
+
+    // fuck
+    let day_dir = path
+    .parent()
+    .unwrap()
+    .iter()
+    .last().unwrap().to_str().unwrap();
+
+    let day = day_dir.parse::<i32>().expect("Failed to parse day");
+
+    println!("Running solution for day {}", day);
+
+    // feed it to the solution
+    match day {
+        1 => {
+            day1::part1(input.as_str());
+            todo!("part 2");
+        },
+        _ => {
+            println!("Day {} is not implemented", day);
+            std::process::exit(1);
+        }
+    }
+
+    Ok(())
 }
